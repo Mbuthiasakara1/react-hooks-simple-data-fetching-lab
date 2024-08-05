dcf10c1a4378f5871c4fc06f57901c970fe2db4a
Mbuthiasakara1 <mbuthiaryne@gmail.com>
2024-08-05 21:19:33 +0300

    dogs lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App(){
+ const [image,setImage]=useState('')
+  const[loading,setLoading]=useState(true)
+
+  useEffect(() =>
+    fetch('https://dog.ceo/api/breeds/image/random')
+  .then(r=>r.json())
+  .then(data =>{
+    setImage(data.message)
+    setLoading(false)
+  }
+    
+  )
+)
+return (
+  <>
+    {loading ? <p>Loading...</p> : <img src={image} alt="A Random Dog"/>}
+  </>
+);
+}
+
+export  default App
